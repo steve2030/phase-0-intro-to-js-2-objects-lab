4446c65a682f9603a8fbcf92d6845e91c20be1f8
steve <stevemighty2030@gmail.com>
2022-08-22 16:19:02 +0300

    the assignment is complete

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: 'New York'
+  }
+
+
+  // used spread operator to clone employee object
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+      ...employee,
+      [key]: value,
+    };
+  };
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {
+      ...employee
+    }
+    delete newEmployee[key];
+    return newEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
